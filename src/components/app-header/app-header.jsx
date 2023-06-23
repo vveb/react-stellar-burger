3d86d5361bc934ff82e45ca7a5933b469de86117
Victor Tarapata <vvteach@yandex.ru>
2023-06-23 19:35:03 +0300

    feat: add AppHeader component

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,19 @@
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
+
+const AppHeader = () => {
+  return(
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.menuButtons}>
+          <a className={styles.link}><BurgerIcon type='primary' /><span className={styles.text}>Конструктор</span></a>
+          <a className={styles.link_inactive}><ListIcon type='secondary' /><span className={styles.text}>Лента заказов</span></a>
+        </div>
+        <Logo />
+        <a className={styles.lk}><ProfileIcon type='secondary' /><span className={styles.text}>Личный кабинет</span></a>
+      </nav>
+    </header>
+  )
+}
+
+export default AppHeader
