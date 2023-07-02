eb5f7682012223a9dcec9028f08a81408a21f5fe
Victor Tarapata <vvteach@yandex.ru>
2023-07-02 18:16:01 +0300

    refact: add hover effects in AppHeader, ingredient price

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -6,11 +6,11 @@ const AppHeader = () => {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <div className={styles.menuButtons}>
-          <a className={styles.link}><BurgerIcon type='primary' /><span className={styles.text}>Конструктор</span></a>
-          <a className={styles.link_inactive}><ListIcon type='secondary' /><span className={styles.text}>Лента заказов</span></a>
+          <a className={styles.link} href='#'><BurgerIcon type='primary' /><span className={styles.text}>Конструктор</span></a>
+          <a className={styles.link_inactive} href='#'><ListIcon type='secondary' /><span className={styles.text}>Лента заказов</span></a>
         </div>
-        <Logo />
-        <a className={styles.lk}><ProfileIcon type='secondary' /><span className={styles.text}>Личный кабинет</span></a>
+        <a className={styles.logoLink} href='#'><Logo /></a>
+        <a className={styles.lk} href='#'><ProfileIcon type='secondary' /><span className={styles.text}>Личный кабинет</span></a>
       </nav>
     </header>
   )
